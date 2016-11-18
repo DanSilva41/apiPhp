932b501e6136615a774da515a41fcfd2304801c6
Danilo Silva <dansilva41@hotmail.com>
2016-11-18 18:00:39 -0200

    Aperfeiçoando a main page

--- index.php
+++ index.php
@@ -10,60 +10,44 @@
 </head>
 <body>
     <?php
+
     $arquivo = 'storage.txt';
+        $str = file_get_contents($arquivo);
+        $array_updateId = explode( ',' , $str);
                 
-        // Gerar numeros aleatorios da megasena
-    for($j = 1; $j <=6;$j++) {
-            $numMegasena[] = rand(1,60);
-            
-    }
-    sort($numMegasena);
-    $str = implode(" - ", $numMegasena);
-                
-    function sendMessage($chatId,$text){
-        file_get_contents("https://api.telegram.org/bot242158604:AAHsZgkHuWC4ZBP3eBNTvuX7_eITmIdunys/sendMessage?chat_id=".$chatId."&text=".$text);
-    }
     $url = "https://api.telegram.org/bot242158604:AAHsZgkHuWC4ZBP3eBNTvuX7_eITmIdunys/getUpdates";
     $content = file_get_contents($url);
     $to = json_decode($content,true);
     //Count
     $run = count($to["result"]);
+
     for($i = 0; $i < $run; $i++){
         $id = $to["result"][$i]["message"]["chat"]["id"];
         $text = $to["result"][$i]["message"]["text"];
-        $idText = $to["result"][$i]["update_id"];
+        $update_id = $to["result"][$i]["update_id"];
             
         $ids[$i] = $id;
         $texts[$i] = $text; 
-    }
-        
-    $ids = array_unique($ids);
-    $ids = array_values($ids);
-    $end = count($ids);
-    //Contando a quantidade de texto
-    $msg = count($texts);
-        
-    for ($i=0; $i < $end ; $i++) { 
-            for($m = 0; $m < $msg; $m++){
-        if($texts[$m] == "/megasena"){
-                    sendMessage($ids[$i],"NÃºmeros da MegaSena:     ".$str);
-                        file_put_contents($arquivo, $idText." , " , FILE_APPEND);
-        }
-        else {
-                    sendMessage($ids[$i],"Ola");
-        }
-            }
+                $updateIds[$i] = $update_id; 
+                
+                if(!in_array($updateIds[$i], $array_updateId)){
+                    if($texts[$i] == '/megasena'){
+                        
+                        $numMegasena = array();
+                         // Gerar numeros da megasena
+                        for($j = 1; $j <=6;$j++) {
+                            $numMegasena[] = rand(1,60);
+                        }
+                        sort($numMegasena);
+                        $sena = implode(" - ", $numMegasena);
+  
+                        $new_url = 'https://api.te'
+                                . 'legram.org/bot242158604:AAHsZgkHuWC4ZBP3eBNTvuX7_eITmIdunys/sendMessage?chat_id='.$ids[$i]."&text=$sena";
+                        file_get_contents($new_url);
+                        file_put_contents($arquivo, $updateIds[$i].",", FILE_APPEND);   
+                    }
+                }
         }   
     ?>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
